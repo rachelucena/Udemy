1aefada0b5d41f66353bed94f4190727f9e7f502
rachelucena <159203793+rachelucena@users.noreply.github.com>
2025-03-25 10:18:57 -0400

    Secao 3
    Aulas 1 e 2

---
+++ Javascript/Secao3-Logica/Aula2-Logicos/index.js
@@ -0,0 +1,6 @@
+/* Operadores logicos: checam mais de uma comparacao
+&& -> AND -> E todas as expressoes precisam ser verdadeiras para retornar true
+|| -> OR -> OU uma ou outra expressao precisa ser verdadeira para retornar true
+! -> NOT -> NAO
+*/
+
