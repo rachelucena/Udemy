9d41ed0963a3af58f4d854ad1de7c7f6b64fad13
rachelucena <159203793+rachelucena@users.noreply.github.com>
2025-03-25 09:46:30 -0400

    Secao 2
    Aula 20

---
+++ Javascript/Secao3-Logica/index.js
@@ -0,0 +1,12 @@
+// Operadores de comparacao
+// > maior que
+// >= maior que ou igual a
+// < menor que
+// <= menor que ou igual a
+// == igualdade (= atribuicao) (valor)
+// === igualdade estrita (valor e tipo)
+// != diferente (valor)
+// !== diferente estrita (valor e tipo)
+
+
+
