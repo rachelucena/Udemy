1fe6c7f0471ee890e4d6e60342952f2c3eb28001
rachelucena <159203793+rachelucena@users.noreply.github.com>
2025-03-26 17:34:40 -0400

    Secao 3
    Aula 5

---
+++ Javascript/Secao3-Logica/Aula5-Exercicio/Assets/JS/main.js
@@ -0,0 +1,8 @@
+/*
+prompt pro usuario colocar os dados no formulario
+reter os valores digitados em variaveis
+fazer o calculo
+passar pelas checagens
+colocar o resultado na tela
+se algum campo ficar vazio, mandar msg de erro
+*/
