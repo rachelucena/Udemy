290be39cd6ac02dd05f2d78ad78ed97df52dd3de
rachelucena <159203793+rachelucena@users.noreply.github.com>
2025-04-02 12:23:57 -0400

    Secao 3
    Correcao aula 20

--- Javascript/Secao3-Logica/Aula20-Exercicio(Logica)/correcao3.js
+++ Javascript/Secao3-Logica/Aula20-Exercicio(Logica)/correcao3.js
@@ -1,9 +1,9 @@
 function fizzBuzz(n) {
     if (typeof n !== 'number') return n;
-    else if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
+    if (n % 3 === 0 && n % 5 === 0) return 'FizzBuzz';
     if (n % 3 === 0) return 'Fizz';
-    else if (n % 5 === 0) return 'Buzz';
-    else return n;
+    if (n % 5 === 0) return 'Buzz';
+    return n;
 }
 
 for (let i = 0; i <= 100; i++) {
